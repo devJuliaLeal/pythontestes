092e0bc866a3e8d8e5b0288dfbe56a34eca78533
Julia Leal <120651477+devJuliaLeal@users.noreply.github.com>
2023-02-23 11:28:38 -0300

    commit

--- tuples.py
+++ tuples.py
@@ -1,6 +1,7 @@
 # tuples: ordered, immutable, allows to duplicate elements
 
-myTuples = ("Max", 28, "Min")
+a = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+b = a[::-1]
+print(b)
 
-    
-print(myTuples.index(28))
+
